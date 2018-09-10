9b1ca0963e12e78cd61d02011152a54d7b8bb6c1
0xboobface <0xboobface@gmail.com>
2018-09-10 18:17:42 +0200

    Reduce log severity for continuity errors to WARN

--- src/main/java/org/taktik/mpegts/sources/BlockingMultiMTSSource.java
+++ src/main/java/org/taktik/mpegts/sources/BlockingMultiMTSSource.java
@@ -48,7 +48,7 @@ public class BlockingMultiMTSSource extends AbstractMTSSource implements AutoClo
             try {
                 continuityFixer.fixContinuity(packet);
             } catch(Exception e) {
-                LOG.error("Failed to fix continuity. MTSPacket probably invalid", e);
+                LOG.warn("Failed to fix continuity. MTSPacket probably invalid", e);
                 return nextPacketInternal();
             }
         }

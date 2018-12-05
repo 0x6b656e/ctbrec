022997f6b61e25d5a74d7039e7effc32cd4f2312
0xboobface <0xboobface@gmail.com>
2018-12-05 21:00:19 +0100

    Add new event property OLD

--- common/src/main/java/ctbrec/EventBusHolder.java
+++ common/src/main/java/ctbrec/EventBusHolder.java
@@ -8,11 +8,26 @@ import com.google.common.eventbus.EventBus;
 public class EventBusHolder {
 
     public static final String EVENT = "event";
+    public static final String OLD = "old";
     public static final String STATUS = "status";
     public static final String MODEL = "model";
 
     public static enum EVENT_TYPE {
+        /**
+         * This event is fired every time the OnlineMonitor sees a model online
+         * It is also fired, if the model was online before. You can see it as a "still online ping".
+         */
+        MODEL_ONLINE,
+
+        /**
+         * This event is fired whenever the model's online state (Model.STATUS) changes.
+         */
         MODEL_STATUS_CHANGED,
+
+
+        /**
+         * This event is fired whenever the state of a recording changes.
+         */
         RECORDING_STATUS_CHANGED
     }
 

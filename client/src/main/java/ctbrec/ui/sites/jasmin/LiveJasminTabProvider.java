32099eec4625e9e40027f086ef0b049ba24b257b
0xboobface <0xboobface@gmail.com>
2018-12-22 22:33:16 +0100

    Add a few more pages

--- client/src/main/java/ctbrec/ui/sites/jasmin/LiveJasminTabProvider.java
+++ client/src/main/java/ctbrec/ui/sites/jasmin/LiveJasminTabProvider.java
@@ -22,11 +22,10 @@ public class LiveJasminTabProvider extends TabProvider {
     public List<Tab> getTabs(Scene scene) {
         List<Tab> tabs = new ArrayList<>();
 
-        long ts = System.currentTimeMillis();
-        LiveJasminUpdateService s = new LiveJasminUpdateService(liveJasmin, liveJasmin.getBaseUrl() + "/en/girls/?listPageOrderType=most_popular&_dc=" + ts);
-        ThumbOverviewTab tab = new ThumbOverviewTab("Girls", s, liveJasmin);
-        tab.setRecorder(liveJasmin.getRecorder());
-        tabs.add(tab);
+        tabs.add(createTab("Girls", liveJasmin.getBaseUrl() + "/en/girls/?listPageOrderType=most_popular"));
+        tabs.add(createTab("Girls HD", liveJasmin.getBaseUrl() + "/en/girls/hd/?listPageOrderType=most_popular"));
+        tabs.add(createTab("Boys", liveJasmin.getBaseUrl() + "/en/boys/?listPageOrderType=most_popular"));
+        tabs.add(createTab("Boys HD", liveJasmin.getBaseUrl() + "/en/boys/hd/?listPageOrderType=most_popular"));
 
         followedTab = new LiveJasminFollowedTab(liveJasmin);
         followedTab.setRecorder(liveJasmin.getRecorder());
@@ -38,5 +37,12 @@ public class LiveJasminTabProvider extends TabProvider {
     @Override
     public Tab getFollowedTab() {
         return followedTab;
+    }
+
+    private ThumbOverviewTab createTab(String title, String url) {
+        LiveJasminUpdateService s = new LiveJasminUpdateService(liveJasmin, url);
+        ThumbOverviewTab tab = new ThumbOverviewTab(title, s, liveJasmin);
+        tab.setRecorder(liveJasmin.getRecorder());
+        return tab;
     }
 }
